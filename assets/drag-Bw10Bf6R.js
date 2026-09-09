7a7fe9e23e1a3315f1712113bc559b791a98c626
xinhai_hong <xinhai_hong@kingdee.com>
2026-09-09 15:19:06 +0800

    Updates

--- assets/drag-Bw10Bf6R.js
+++ assets/drag-Bw10Bf6R.js
@@ -1,4 +1,4 @@
-import{t as e}from"./jsx-runtime-OQpaS_Dv.js";import{t}from"./CloudArrowUp-DzmJJcT6.js";import{t as n}from"./button-WsNqWjZh.js";import{t as r}from"./upload-CnBIU036.js";var i=e();function a(){return(0,i.jsx)(r,{onChange:e=>console.log(`File list:`,e),children:(0,i.jsx)(n,{children:`Click to upload`})})}var o=`import { Upload, Button } from "lingee-ui";
+import{t as e}from"./jsx-runtime-OQpaS_Dv.js";import{t}from"./CloudArrowUp-DzmJJcT6.js";import{t as n}from"./button-WsNqWjZh.js";import{t as r}from"./upload-esBjWFYM.js";var i=e();function a(){return(0,i.jsx)(r,{onChange:e=>console.log(`File list:`,e),children:(0,i.jsx)(n,{children:`Click to upload`})})}var o=`import { Upload, Button } from "lingee-ui";
 
 export default function BasicDemo() {
   return (
--- assets/group-BUbU67j8.js
+++ assets/group-BUbU67j8.js
@@ -1,4 +1,4 @@
-import{t as e}from"./jsx-runtime-OQpaS_Dv.js";import{t}from"./image-OSmXeKO9.js";var n=e();function r(){return(0,n.jsx)(t,{src:`https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png`,alt:`Sample image`,width:200,height:200,style:{objectFit:`cover`,borderRadius:8}})}var i=`import { Image } from "lingee-ui";
+import{t as e}from"./jsx-runtime-OQpaS_Dv.js";import{t}from"./image-5b1dI3sW.js";var n=e();function r(){return(0,n.jsx)(t,{src:`https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png`,alt:`Sample image`,width:200,height:200,style:{objectFit:`cover`,borderRadius:8}})}var i=`import { Image } from "lingee-ui";
 
 export default function BasicDemo() {
   return (
